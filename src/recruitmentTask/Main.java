45f018dc884f2174693771d0869e331f56e6034e
Rafal1 <rafal.zawadzki3@gmail.com>
2015-03-03 13:07:03 +0100

    time limit for filling queue has been added (to properties and code);

--- src/recruitmentTask/Main.java
+++ src/recruitmentTask/Main.java
@@ -5,6 +5,7 @@ import recruitmenttask.taskproducer.TaskProducerThread;
 
 import java.io.*;
 import java.util.Properties;
+import java.util.concurrent.*;
 
 /**
  * @author Rafał Zawadzki
@@ -33,7 +34,7 @@ public class Main {
             }
         } else {
             System.out.println("You didn't give any argument.");
-            System.out.println("In addition, If you hadn't specified the queue length value in the config file. The default would be 100.");
+            System.out.println("If you hadn't specified the queue length value in the config file. The default would be 100.");
             Integer queueN = Integer.parseInt(prop.getProperty(PropertiesEnum.MAX_QUEUE_LENGTH.name()));
             initializeQueue(queueN);
         }
@@ -48,18 +49,27 @@ public class Main {
         Integer producersN = Integer.parseInt(prop.getProperty(PropertiesEnum.NUMBER_OF_TASK_PRODUCERS.name()));
         Integer consumersN = Integer.parseInt(prop.getProperty(PropertiesEnum.NUMBER_OF_TASK_CONSUMERS.name()));
 
+        ExecutorService exec = Executors.newCachedThreadPool();
+
         for (int i = 0; i < producersN; i++) {
-            Thread tp = new Thread(new TaskProducerThread());
-            tp.start();
+            exec.execute(new TaskProducerThread());
         }
 
         // wait until the queue becomes full (it's a task requirement)
+        Long startTime = System.currentTimeMillis();
+        Long timeLimit = new Long(prop.getProperty(PropertiesEnum.FILLING_QUEUE_TIME_LIMIT.name()));
         while (true) {
             if (FirstQueue.getInstance().isFull()) {
                 for (int i = 0; i < consumersN; i++) {
-                    Thread tc = new Thread(new TaskConsumerThread());
-                    tc.start();
+                    exec.execute(new TaskConsumerThread());
                 }
+                break;
+            }
+
+            Long currentTime = System.currentTimeMillis();
+            Long spendTime = currentTime - startTime;
+            if(spendTime >= timeLimit) {
+                System.out.println("Time limit for filling queue has been reached.");
                 break;
             }
         }
@@ -103,11 +113,14 @@ public class Main {
             Integer numberOfTaskConsumers = 4;
             Integer maxQueueLength = 100;
             Integer maxLengthOfTasksString = 100;
+            Long fillingQueueTimeLimit = new Long(1000 * 60 * 1); //mnutes in miliseconds
 
             prop.setProperty(PropertiesEnum.NUMBER_OF_TASK_PRODUCERS.name(), numberOfTaskProducers.toString());
             prop.setProperty(PropertiesEnum.NUMBER_OF_TASK_CONSUMERS.name(), numberOfTaskConsumers.toString());
             prop.setProperty(PropertiesEnum.MAX_QUEUE_LENGTH.name(), maxQueueLength.toString());
             prop.setProperty(PropertiesEnum.MAX_LENGTH_OF_TASKS_STRING.name(), maxLengthOfTasksString.toString());
+            prop.setProperty(PropertiesEnum.MAX_LENGTH_OF_TASKS_STRING.name(), maxLengthOfTasksString.toString());
+            prop.setProperty(PropertiesEnum.FILLING_QUEUE_TIME_LIMIT.name(), fillingQueueTimeLimit.toString());
 
             prop.store(output, null);
 
